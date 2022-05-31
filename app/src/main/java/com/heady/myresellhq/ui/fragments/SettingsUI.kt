f9ef2cf72c149fa400248f2b4af2980c0d2b752c
edwar <javaeddie84@gmail.com>
2022-05-31 00:12:55 -0500

    finished creating fragments and their layouts

---
+++ app/src/main/java/com/heady/myresellhq/ui/fragments/SettingsUI.kt
@@ -0,0 +1,10 @@
+package com.heady.myresellhq.ui.fragments
+
+import androidx.fragment.app.Fragment
+import com.heady.myresellhq.R
+
+
+class SettingsUI : Fragment(R.layout.fragment_settings_u_i) {
+
+
+}
---
+++ app/src/main/java/com/heady/myresellhq/ui/fragments/StatsUI.kt
@@ -0,0 +1,10 @@
+package com.heady.myresellhq.ui.fragments
+
+import androidx.fragment.app.Fragment
+import com.heady.myresellhq.R
+
+
+class StatsUI : Fragment(R.layout.fragment_stats_u_i) {
+
+
+}
